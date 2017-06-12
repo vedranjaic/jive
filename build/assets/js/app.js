e865012151aa4cb7ccfb7a4ce4c4d3e1647422b6
Vedran Jaic <vedranjaic@icloud.com>
2017-06-13 01:53:10 +0200

    Final stage

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -1,7 +1,7 @@
 // Accordion collapse
 var $myGroup = $('#menu-accordion');
-$myGroup.on('show.bs.collapse','.collapse', function() {
-    $myGroup.find('.collapse.in').collapse('hide');
+	$myGroup.on('show.bs.collapse','.collapse', function() {
+	$myGroup.find('.collapse.in').collapse('hide');
 });
 
 // Tooltips
@@ -22,79 +22,25 @@ $('.toggle-menu').click(function() {
 
 // Menu tabs toggle
 $('.nav-tabs a').click(function(e) {
-    $('.nav-tabs .active').removeClass('active');
-    var $this = $(this).parent();
-    if (!$this.hasClass('active')) {
-        $this.addClass('active');
-    }
-    // e.preventDefault();
+	$('.nav-tabs .active').removeClass('active');
+	var $this = $(this).parent();
+	if (!$this.hasClass('active')) {
+		$this.addClass('active');
+	}
+	// e.preventDefault();
 });
 
-// Submmenu toggle
-// $('.submenu-item a').click(function(e) {
-//     $('.submenu-item.active').removeClass('active');
-//     var $this = $(this).parent();
-//     if (!$this.hasClass('active')) {
-//         $this.addClass('active');
-//     }
-//     e.preventDefault();
-// });
-
-// Submenu links
-$('.step02').click(function() {
-	$('.submenu-item.active').removeClass('active');
-	$('.submenu-item .step02').parent().addClass('active');
-	// Hide all others
-	$('#03-finalize-business-plan').addClass('hidden');
-	$('#04-decide-fso-packaged-solutions-model-and-target-function').addClass('hidden');
-	$('#05-finalize-team-structure-and-assign-roles-responsibilities').addClass('hidden');
-	$('#06-finalize-training-plan').addClass('hidden');
-	// Show the one we want
-	$('#02-attend-cloud-onboarding-session').removeClass('hidden');
-});
-$('.step03').click(function() {
-	$('.submenu-item.active').removeClass('active');
-	$('.submenu-item .step03').parent().addClass('active');
-	// Hide all others
-	$('#02-attend-cloud-onboarding-session').addClass('hidden');
-	$('#04-decide-fso-packaged-solutions-model-and-target-function').addClass('hidden');
-	$('#05-finalize-team-structure-and-assign-roles-responsibilities').addClass('hidden');
-	$('#06-finalize-training-plan').addClass('hidden');
-	// Show the one we want
-	$('#03-finalize-business-plan').removeClass('hidden');
-});
-$('.step04').click(function() {
-	$('.submenu-item.active').removeClass('active');
-	$('.submenu-item .step04').parent().addClass('active');
-	// Hide all others
-	$('#02-attend-cloud-onboarding-session').addClass('hidden');
-	$('#03-finalize-business-plan').addClass('hidden');
-	$('#05-finalize-team-structure-and-assign-roles-responsibilities').addClass('hidden');
-	$('#06-finalize-training-plan').addClass('hidden');
-	// Show the one we want
-	$('#04-decide-fso-packaged-solutions-model-and-target-function').removeClass('hidden');
-});
-$('.step05').click(function() {
-	$('.submenu-item.active').removeClass('active');
-	$('.submenu-item .step05').parent().addClass('active');
-	// Hide all others
-	$('#02-attend-cloud-onboarding-session').addClass('hidden');
-	$('#03-finalize-business-plan').addClass('hidden');
-	$('#04-decide-fso-packaged-solutions-model-and-target-function').addClass('hidden');
-	$('#06-finalize-training-plan').addClass('hidden');
-	// Show the one we want
-	$('#05-finalize-team-structure-and-assign-roles-responsibilities').removeClass('hidden');
-});
-$('.step06').click(function() {
-	$('.submenu-item.active').removeClass('active');
-	$('.submenu-item .step06').parent().addClass('active');
-	// Hide all others
-	$('#02-attend-cloud-onboarding-session').addClass('hidden');
-	$('#03-finalize-business-plan').addClass('hidden');
-	$('#04-decide-fso-packaged-solutions-model-and-target-function').addClass('hidden');
-	$('#05-finalize-team-structure-and-assign-roles-responsibilities').addClass('hidden');
-	// Show the one we want
-	$('#06-finalize-training-plan').removeClass('hidden');
+// Table first-child / not-first-child
+$.each( $('table', this.$el), function( index, table ){
+	var rowspan = 0;
+	$.each( $('tr', table), function( index, tr ){
+		if( rowspan > 0 ){
+			$('td:first-child', tr).addClass("not-first-child");
+			rowspan = (rowspan>0) ? rowspan-1 : 0;
+		} else if( $('td:first-child', tr).attr("rowspan") > 0){
+			rowspan = parseInt( $('td:first-child', tr).attr("rowspan")) - 1;
+		}
+	});
 });
 
 
